4e213a42693a11d24bc919646aa30882b3c61d7b
faresSultan <fares.sultan9@gmail.com>
2024-07-04 12:43:14 +0300

    .

--- Session1_labs/Assignment/Assignment1.v
+++ Session1_labs/Assignment/Assignment1.v
@@ -104,15 +104,22 @@
 
 /*---------------------------------ASSIGNMENT1_EXTRA---------------------------------*/
 
-/*-----Q1-----*/
-module F (A,out);
+// /*-----Q1-----*/
+// module F (A,out);
 
-input [3:0] A;
-output out;
+// input [3:0] A;
+// output out;
 
-    and(AND1, A[2], !A[3]);
-    and(AND2, !A[3], A[1], A[0]);
-    or(out,AND1,AND2);
+//     and(AND1, A[2], !A[3]);
+//     and(AND2, !A[3], A[1], A[0]);
+//     or(out,AND1,AND2);
+  
+// endmodule
 
+/*-----Q3-----*/
+
+module F (
+    ports
+);
     
 endmodule
