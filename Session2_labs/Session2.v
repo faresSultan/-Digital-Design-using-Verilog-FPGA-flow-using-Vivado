6d5d43f34d7d6ffbbbfef8d7a4a0842a958b76e6
faresSultan <fares.sultan9@gmail.com>
2024-07-05 09:29:05 +0300

    Session 2 lab 1

---
+++ Session2_labs/Session2.v
@@ -0,0 +1,23 @@
+
+
+module N_Bit_ALU(A,B,ALU_OP,RES);
+
+parameter N = 4 ;
+input [N-1:0] A,B;
+input [1:0] ALU_OP;
+output reg [N-1:0] RES;
+
+always @(*) begin
+case ({ALU_OP[1],ALU_OP[0]}):
+
+    2'b00: RES = A+B;
+    2'b01: RES = A-B;
+    2'b10: RES = A|B;
+    2'b11: RES = A^B;
+    default: RES = 0;   
+endcase
+end
+
+
+    
+endmodule
