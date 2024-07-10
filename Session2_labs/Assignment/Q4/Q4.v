f4e2c7b4fa8edc52f25512640e15783c61b93856
faresSultan <fares.sultan9@gmail.com>
2024-07-10 19:34:17 +0300

    Q4 passed testbench

--- Session2_labs/Assignment/Q4/Q4.v
+++ Session2_labs/Assignment/Q4/Q4.v
@@ -1,6 +1,9 @@
 
-module moduleName (
-    ports
-);
+module N_bit_adder #(parameter N = 1) (A,B,C);
+
+input [N-1:0] A,B;
+output [N-1:0] C;
+
+assign C = A+B;
     
 endmodule
--- Session2_labs/Assignment/Q4/Q4_tb.v
+++ Session2_labs/Assignment/Q4/Q4_tb.v
@@ -1,5 +1,30 @@
-module moduleName (
-    ports
-);
+module Q4_tb ();
+    
+    reg  A_tb,B_tb;
+    wire C_tb;
+    reg C_exp;
+
+    N_bit_adder adder_tb (.A(A_tb),.B(B_tb),.C(C_tb));
+integer i;
+
+    initial begin
+
+        for(i=0;i<20;i = i+1)begin
+            A_tb = $random;
+            B_tb = $random;
+            
+            C_exp = A_tb + B_tb;
+
+            #10; 
+            if (C_tb != C_exp) begin
+                $display("Error - Output is incorrect\n");
+                #10;
+                $stop; 
+            end
+              
+        end
+        $display("Passed");
+            $stop;
+    end
     
 endmodule
