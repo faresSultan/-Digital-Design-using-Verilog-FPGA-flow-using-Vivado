62670990d0045be9dc6dc02a9cc64516973a281f
faresSultan <fares.sultan9@gmail.com>
2024-07-12 14:16:23 +0300

    lab2

---
+++ Session3_labs/dff_en_pre_tb.v
@@ -0,0 +1,43 @@
+module dff_en_pre_tb();
+    
+    reg clk;
+    reg d_tb,e_tb,pre_tb;
+    wire q_tb;
+
+    initial begin
+        clk = 0;
+
+        forever begin
+            #1 clk = ~clk;
+        end
+    end
+        dff_E_PRE DUT (.D(d_tb),.E(e_tb),.PRE(pre_tb),.CLK(clk),.Q(q_tb));
+    initial begin
+        pre_tb = 0;
+        d_tb = 0;
+        e_tb = 0;
+        @(negedge clk);
+        pre_tb = 1;
+
+        repeat (10) begin
+            d_tb = $random;
+        @(negedge clk);
+        end
+
+        e_tb = 1;
+        repeat (50) begin
+            d_tb = $random;
+        @(negedge clk);
+        end
+
+        repeat (10) begin
+            d_tb = $random;
+            e_tb = $random;
+        @(negedge clk);
+        end
+        $stop;
+        end
+
+        endmodule
+
+
--- Session3_labs/s3.v
+++ Session3_labs/s3.v
@@ -1,18 +1,37 @@
-module dff_E_PRE (D,E,PRE,CLK,Q);
+// module dff_E_PRE (D,E,PRE,CLK,Q);
 
-    input D,E,PRE,CLK;
-    output reg Q;
+//     input D,E,PRE,CLK;
+//     output reg Q;
 
-    always @(negedge PRE or posedge CLK) begin
+//     always @(negedge PRE or posedge CLK) begin
 
-        if (~PRE) begin
-            Q <= 1;
-        end
-        else if (E) begin
-            Q <= D;
-        end
-        // else Q=Q is meaningless
+//         if (~PRE) begin
+//             Q <= 1;
+//         end
+//         else if (E) begin
+//             Q <= D;
+//         end
+//         // else Q=Q is meaningless
         
+//     end
+    
+// endmodule
+
+
+
+module shift_r (C,SI,LEFT_RIGHT,PO);
+
+    input C,SI,LEFT_RIGHT;
+    output [7:0] PO;
+
+    always @(posedge C) begin
+        
+        if (LEFT_RIGHT) begin
+            PO <= {SI,PO[7:1]};
+        end
+        else begin
+            PO <= {PO[6:0],SI};
+        end
     end
     
 endmodule
