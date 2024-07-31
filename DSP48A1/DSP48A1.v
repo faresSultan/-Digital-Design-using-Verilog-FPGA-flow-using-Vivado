868a29aab469a06ff3d0077f00b843c574f4eb9f
faresSultan <fares.sultan9@gmail.com>
2024-08-01 01:24:03 +0300

    pre-adder/subtractor stage code, written but not yet tested

--- DSP48A1/DSP48A1.v
+++ DSP48A1/DSP48A1.v
@@ -34,21 +34,41 @@ module DSP48A1 #(
     output [35:0] M;
     output CARRYOUT, CARRYOUTF;
 
+
+// opmode reg init
+    wire OPMODE_r;
+    VAL_REG_MUX#(.N(8)) OPMODE_REG (.val(OPMODE),.sel(OPMODEREG),.rst(RSTOPMODE),.CE(CEOPMODE),.clk(clk),.mux_out(OPMODE_r));
+
+// pre-adder/subtractor stage
+    wire D_r,B0_in,B0_r;
+    reg preStage_out;
+    assign B0_in = (B_INPUT == "DIRECT")? B:((B_INPUT == "CASCADE")? BCIN : 0)  
+
+    VAL_REG_MUX#(.N(18)) D_REG (.val(D),.sel(DREG),.rst(RSTD),.CE(CED),.clk(clk),.mux_out(D_r));
+    VAL_REG_MUX#(.N(18)) B_REG (.val(B0_in),.sel(BREG),.rst(RSTB),.CE(CEB),.clk(clk),.mux_out(B0_r));
+
+    always @(B0_r,D_r,OPMODE[6]) begin
+        if (OPMODE[6] == 0) preStage_out = D_r + B0_r;
+        else if (OPMODE[6] == 1) preStage_out = D_r - B0_r;
+    end
+
+
 endmodule
 
 
 
-module VAL_REG_MUX #(parameter N = 1) (val,sel,clk,mux_out);
+module VAL_REG_MUX #(parameter N = 1) (val,sel,rst,CE,clk,mux_out);
 
     input [N-1:0] val;
-    input clk,sel;
+    input clk,rst,sel.CE;
     output [N-1:0] mux_out;
     wire val_r;
 
     always @(posedge clk) begin
-        val_r = val;
+        if(rst) val_r = 0;
+        else if (CE) val_r = val;
     end    
      
-     assign (sel == 1)? (mux_out = val_r):(mux_out = val);
+     assign mux_out = (sel == 1)? val_r:val;
     
 endmodule
